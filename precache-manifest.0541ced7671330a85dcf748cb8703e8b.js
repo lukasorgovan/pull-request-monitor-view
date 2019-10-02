d9c323f00343d0d5cf6aa0677efb0f6358ae0229
lukasorgovan <lukasorgovan@users.noreply.github.com>
2019-10-02 21:37:21 +0000

    Built artifacts of a94ce1f [ci skip]

---
+++ precache-manifest.0541ced7671330a85dcf748cb8703e8b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "71c912b22cc42ee749c0",
+    "url": "/static/js/main.71c912b2.chunk.js"
+  },
+  {
+    "revision": "5c3dde4c7920615ff52a",
+    "url": "/static/js/1.5c3dde4c.chunk.js"
+  },
+  {
+    "revision": "71c912b22cc42ee749c0",
+    "url": "/static/css/main.7d950c7e.chunk.css"
+  },
+  {
+    "revision": "7131b2b53d16dc428b888376d6bb9db2",
+    "url": "/index.html"
+  }
+];
